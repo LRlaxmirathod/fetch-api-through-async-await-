c43d323ccc9f207744579366aeecc78580e9e84b
LRlaxmirathod <168898815+LRlaxmirathod@users.noreply.github.com>
2024-07-29 11:55:03 +0530

    fetch api then catch
    fetch then catch

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-//using  async await 
+// fetch api using then catch//
 
 let button = document.querySelector("#button")
 let p = document.querySelector("#p")
 
 let url = "https://catfact.ninja/fact";
 
-async  function get(){
-    let response = await fetch(url);
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    p.innerText = data.fact;
+function get(){
+    fetch(url)
+    .then((response)=>{
+        console.log(response)
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data)
+        p.innerText = data.fact
+    })
+
 }
 
-button.addEventListener('click'  , function(){
-get()
-
+button.addEventListener('click' , function(e){
+    console.log("button was clicked")
+    get()
+    console.log(e.target)
+    console.log(e.type)
+    console.log(e.timeStamp)
+    console.log(e.bubbles)
+    console.log(e.currentTarget)
+    console.log(e.clientX)
+    console.log(e.clientY)
 })
+
 
 
